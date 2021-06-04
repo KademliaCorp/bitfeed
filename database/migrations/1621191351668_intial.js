abb318899198a754559f1e44badcf4da01ecea9a
Hedzer <hedzer87@gmail.com>
2021-06-04 00:33:15 -0400

    Added basic seeding

--- database/migrations/1621191351668_intial.js
+++ database/migrations/1621191351668_intial.js
@@ -213,6 +213,18 @@ exports.up = (pgm) => {
 
 	pgm.createIndex('feed_item_map', 'feed_item_id');
 	pgm.createIndex('feed_item_map', 'feed_id');
+
+	console.log(process.env.NODE_ENV);
+	if (process.env.NODE_ENV === "development") {
+		pgm.sql(/* sql */`
+			INSERT INTO
+				"user" (username, password)
+			VALUES 
+				-- username: dev
+				-- password: 123
+				('dev', '$2b$12$DvRjY2mIfBTJZ/PIQDUudeYN0SE.ElQRI8El3WvuDf3RadpEss15e')
+		`);
+	}
 };
 
 exports.down = (pgm) => {

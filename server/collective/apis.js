30032dc6214eef9abb06fd923fcadf04802720e2
Hedzer <hedzer87@gmail.com>
2021-06-20 16:22:45 -0400

    Fixed bad middleware instantiation

--- server/collective/apis.js
+++ server/collective/apis.js
@@ -2,7 +2,7 @@ const signIn = require('../api/sign-in');
 
 module.exports = function routes(app) {
 	const API = 'api'
-	app.get(`/${API}/sign-in`, signIn);
+	app.use(`/${API}/sign-in`, signIn);
 
 	app.use(`/${API}/*`, (req, res, next) => {
 		res.status(404);
